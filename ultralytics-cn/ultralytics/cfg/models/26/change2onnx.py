5fe62bf7cc7c2dd866cb4842e6a60b66b65f44a9
小尛QAQ <3365985488@qq.com>
2026-09-17 21:16:52 +0800

    Delete ultralytics-cn directory
    把修改后的ultralytics建立一个branch

--- ultralytics-cn/ultralytics/cfg/models/26/change2onnx.py
+++
@@ -1,3 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("yolo26n.yaml")
-model.export(format="onnx")
--- ultralytics-cn/ultralytics/models/yolo/semantic/__init__.py
+++
@@ -1,7 +0,0 @@
-# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-
-from .predict import SemanticSegmentationPredictor
-from .train import SemanticSegmentationTrainer
-from .val import SemanticSegmentationValidator
-
-__all__ = "SemanticSegmentationPredictor", "SemanticSegmentationTrainer", "SemanticSegmentationValidator"
