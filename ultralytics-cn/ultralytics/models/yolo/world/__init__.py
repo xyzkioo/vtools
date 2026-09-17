b5bc4b316f7c5c8a5614223c84693c3217575e53
xyzkioo <xyzkioo@users.noreply.github.com>
2026-09-17 22:25:11 +0800

    将 Ultralytics 拆分为外部可选仓库

--- ultralytics-cn/ultralytics/models/yolo/world/__init__.py
+++
@@ -1,6 +0,0 @@
-# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
-
-from .train import WorldTrainer
-from .val import WorldValidator
-
-__all__ = ["WorldTrainer", "WorldValidator"]
